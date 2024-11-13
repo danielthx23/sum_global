9013a181e937d7e936ee90c3f2623951fceee3f7
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-11-12 21:00:02 -0400

    Fix: Peer dependency react error

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className="bg-gradient-to-tl from-background from-20% to-gradientcolor to-100% w-full h-52 flex flex-col z-[3] justify-center items-center">
+        <footer className="bg-gradient-to-tl from-background from-20% to-gradientcolor to-100% w-full h-52 flex flex-col z-[3] justify-end items-center px-8">
              <ul className=" flex flex-col justify-center items-center gap-8">
                 <Link href={"/"} className="">Sobre</Link>
                 <Link href={"/"} className="">Redes Sociais</Link>

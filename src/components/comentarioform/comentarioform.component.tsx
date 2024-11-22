d1be02c3ce34a8a2d427a72ef03ffd970e9b15ec
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-11-22 14:12:58 -0400

    Fix: Responsiveness

--- src/components/comentarioform/comentarioform.component.tsx
+++ src/components/comentarioform/comentarioform.component.tsx
@@ -98,7 +98,7 @@ async function submitCallback(values: FormState) {
       ref={formRef}
       noValidate
     >
-      <section className="grid grid-cols-3 gap-2 w-full">
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 w-full">
         <Input
           label="Título"
           type="text"
